a7f5b6c1befdf9c0c296c5e4918367bceb2be3c2
Leo <lpenrose@depaul.edu>
2022-07-03 18:19:35 -0500

    updated cors

--- src/main/java/com/example/sushiapi/controller/SushiRollController.java
+++ src/main/java/com/example/sushiapi/controller/SushiRollController.java
@@ -5,6 +5,7 @@ import com.example.sushiapi.service.SushiRollService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/sushirolls")
 @AllArgsConstructor
 public class SushiRollController {
@@ -24,10 +26,6 @@ public class SushiRollController {
         return sushiRollService.getAllSushirolls();
     }
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello";
-    }
 
 }
 
