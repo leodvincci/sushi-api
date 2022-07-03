c8db822db742ca3e561c7f0c07511bdf4702696b
Leo <lpenrose@depaul.edu>
2022-07-03 12:35:35 -0500

    updated configs

--- src/main/java/com/example/sushiapi/SushiApiApplication.java
+++ src/main/java/com/example/sushiapi/SushiApiApplication.java
@@ -18,7 +18,7 @@ public class SushiApiApplication {
     CommandLineRunner runner(SushiRollRepo sushiRollRepo){
         return args -> {
 
-            SushiRoll sushiRoll = new SushiRoll(143," roll","it's really good");
+            SushiRoll sushiRoll = new SushiRoll(89," roll","it's really good");
 
             sushiRollRepo.insert(sushiRoll);
         };
